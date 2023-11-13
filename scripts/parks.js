2f756c27191f8521359541c7317a86a96f0d667d
Bradley Yeager <drifterdubz@gmail.com>
2023-11-13 11:30:03 -0500

    got filter by state working

--- scripts/parks.js
+++ scripts/parks.js
@@ -1,20 +1,13 @@
-const parksDrop = document.querySelector("#parksDrop");
+const statesDrop = document.querySelector("#statesDrop");
 const parkTableDataBody = document.querySelector("#parkTableDataBody");
 
-function loadParkList() {
-  for (const Park of nationalParksArray) {
+function loadStateList() {
+  for (const locationName of locationsArray) {
     let option = document.createElement("option");
-    option.textContent = Park.State;
-    parksDrop.appendChild(option);
+    option.textContent = locationName;
+    statesDrop.appendChild(option);
   }
 }
-
-function loadDefaultOption() {
-    let option = document.createElement("option");
-    option.textContent = "Select...";
-    option.value = "";
-    activityList.appendChild(option);
-  }
 
 
 function loadParksTable() {
@@ -35,6 +28,32 @@ function loadParksTable() {
   }
 }
 
+function loadParksByState(State) {
+  parkTableDataBody.innerHTML = "";
+  for (const park of nationalParksArray) {
+    if (park.State == State) {
+        let row = parkTableDataBody.insertRow(-1);
+        let cell = row.insertCell(0);
+        cell.innerText = park.LocationID;
+        let cell2 = row.insertCell(1);
+        cell2.innerText = park.LocationName;
+        let cell3 = row.insertCell(2);
+        cell3.innerText = park.City;
+        let cell4 = row.insertCell(3);
+        cell4.innerText = park.State;
+        let cell5 = row.insertCell(4);
+        cell5.innerText = park.ZipCode;
+        let cell6 = row.insertCell(5);
+        cell6.innerText = park.Phone;
+    }
+  }
+}
 
-loadParkList();
+function stateListChanged() {
+  const stateName = statesDrop.value;
+  loadParksByState(stateName);
+}
+
+loadStateList();
 loadParksTable();
+statesDrop.onchange = stateListChanged;
